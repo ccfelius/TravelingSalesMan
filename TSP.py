51edc59ffed82819ff58b98c67b9c368d66b1faa
ccfelius <lottefelius@Hotmail.com>
2020-12-04 20:27:38 +0100

    mooi plotje van kortste route

--- TSP.py
+++ TSP.py
@@ -2,10 +2,13 @@
 
 # Imports
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 
 # read data from file
-f = open("TSP-configurations/eil51.tsp.txt", "r")
+filename = "eil51.tsp"
+f = open(f"TSP-configurations/{filename}.txt", "r")
 network = f.readlines()[6:-1]
 
 # create dictionary to store coordinates
@@ -19,14 +22,29 @@ for node in network:
 x = [x[0] for x in nodes.values()]
 y = [y[1] for y in nodes.values()]
 
+# load in data of optimal path
+data = pd.read_csv("data/eil51.tsp-batch-1.txt", sep="\t")
+z = list(map(float,list(data['429-10'])))
+temp = []
+# get coordinates of each point
+for item in z:
+    temp.append(nodes[item])
+
+temp = np.array(temp)
+# path = [temp[i:i+2] for i in range(len(temp)-2+1)]
+# print(path)
+
 # Plot the nodes and coordinates
 fig, ax = plt.subplots()
-ax.scatter(x, y)
+ax.scatter(x, y, color="deeppink")
 
 for i, txt in enumerate(nodes.keys()):
     ax.annotate(txt, (x[i], y[i]))
 
-plt.savefig("plots/example.png")
+ax.plot(*temp.T, color="deeppink", alpha=0.5)
+ax.set_title(f"Shortest Route: {filename}", fontsize=16)
+#
+plt.savefig("plots/example-route.png")
 plt.show()
 
 
